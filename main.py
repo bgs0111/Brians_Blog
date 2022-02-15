7a7cae651fa65f7d1269fc368918f5b32a3807dc
Bonggyu Shin <bgs0111@gmail.com>
2022-02-16 06:02:10 +0900

    Replaced SQLite to PostgreSQL 2nd

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ class Comment(db.Model):
     comment_author = relationship("User", back_populates="comments")
     text = db.Column(db.Text, nullable=False)
 
-# db.create_all()
+db.create_all()
 
 
 ## Admin validation
